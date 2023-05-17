0fa98e5485692434354ef1db92a49c5456967e68
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2023-05-17 12:08:05 -0300

    Feat: adding frontend components

---
+++ src/frontend/components/Card/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from 'components/Card/Card.module.scss'
+
+
+
+function Card(props) {
+  return (
+    <div className={styles.main}>
+      <h3>{props.title}</h3>
+      {props.type ? (
+        <div className={styles.time}>
+            <p>10:10</p>
+            </div>
+      ):(
+        <div className={styles.imageBox}>
+            <img src={"/grafico.svg"} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Card;

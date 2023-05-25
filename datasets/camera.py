cad4760ae1ab50f857150f695fa078c5d6e3123b
luanaparra <luana.silva@sou.inteli.edu.br>
2023-05-25 15:52:38 -0300

    update visao comp

--- datasets/camera.py
+++ datasets/camera.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 import cv2 as cv
 
-model = YOLO('./datasets/yolov8n.pt')
+model = YOLO('./datasets/yolov8n2.pt')
 
-cap = cv.VideoCapture(0)
+capture = cv.VideoCapture(0)
 
 while True:
-   _, frame = cap.read()
+   _, frame = capture.read()
    result = model.predict(frame, conf=0.6)
    cv.imshow('frame', result[0].plot())
    if cv.waitKey(1) == ord('q'):

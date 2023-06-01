d6b6b87c01bff5fef5bfca8d24ac8aa183fa7574
Pablo-RLV <pablo.viana@sou.inteli.edu.br>
2023-06-01 17:04:15 -0300

    feat: Reading sensors with scale

--- src/embedded/microcontroller/arduino.ino
+++ src/embedded/microcontroller/arduino.ino
@@ -3,31 +3,33 @@
 const int sensorTempUmi = A1;
 const int sensorGasAnalog = A0;
 const int sensorGasDigi = 7;
-
+int vetor[4];
 dht DHT;
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
-  gas();
-  temp_umi();
-  Serial.println("----------------------");
-  delay(2000);
+    gas();
+    temp_umi();
+    Serial.print(vetor[0]);
+    Serial.print(",");
+    Serial.print(vetor[1]);
+    Serial.print(",");
+    Serial.print(vetor[2]);
+    Serial.print(",");
+    Serial.println(vetor[3]);
+    delay(2000);
 }
 
 void gas() {
-  Serial.print("Valor Gas: ");
-  Serial.println(analogRead(sensorGasAnalog));
-  Serial.print("Valor Gas: ");
-  Serial.println(digitalRead(sensorGasDigi));
+    vetor[0] = analogRead(sensorGasAnalog)/8.3;
+    vetor[1] = digitalRead(sensorGasDigi);
 }
 
 void temp_umi() {
-  DHT.read11(sensorTempUmi);
-  Serial.print("Umidade: ");
-  Serial.println(DHT.humidity);
-  Serial.print("Temperatura: ");
-  Serial.println(DHT.temperature, 0);
+    DHT.read11(sensorTempUmi);
+    vetor[2] = DHT.humidity;
+    vetor[3] = DHT.temperature;
 }

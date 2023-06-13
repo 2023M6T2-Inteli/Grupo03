b05835b4087124cf6d8cd2ffdb984f61f6536e0d
Henrique Marlon <henriquemarloncs@gmail.com>
2023-06-13 13:56:33 -0300

    feat: :art: adding request via websocket on battery subscriber

--- src/backend/ros/modules/battery.py
+++ src/backend/ros/modules/battery.py
@@ -1,20 +1,28 @@
 import rclpy
+import asyncio
+import websockets
 from rclpy.node import Node
 from sensor_msgs.msg import BatteryState
- 
+
+
 class Battery(Node):
     def __init__(self):
-        super().__init__('batterysubscriber')
+        super().__init__('battery_subscriber')
         self.subscription = self.create_subscription(
             BatteryState,
             '/battery_state', 
             self.listener_callback, 
             10)
+        
+    async def send_battery_via_websocket(self, relative_battery_percent):
+        async with websockets.connect("ws://localhost:8000/battery") as websocket:
+            await websocket.send(relative_battery_percent)
 
     def listener_callback(self, battery):
         relative_battery_percent = (battery.voltage - 11)/1.6
-        return relative_battery_percent
-        # O tópico está funcionando, falta apenas fazer a requisição post para o banco de dados.
+
+        asyncio.get_event_loop().run_until_complete(self.send_battery_via_websocket(str(relative_battery_percent)))
+        print(relative_battery_percent)
 
 def main():
     rclpy.init()

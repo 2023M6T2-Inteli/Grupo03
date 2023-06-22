1a30d36648c8cebafe7f122b98a27c46aefd35b4
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2023-06-22 17:07:52 -0300

    Update Front

--- src/frontend/pages/reportPage.jsx
+++ src/frontend/pages/reportPage.jsx
@@ -5,11 +5,13 @@ import React, { useState, useEffect } from "react";
 import Header from "@/components/Header/Header";
 import WebSocketComponent from "@/components/WebSocket/WebSocket";
 import Bateria from "@/components/Bateria/Bateria";
-import { v4 as uuidv4 } from "uuid";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Home({ children }) {
   const addressInputRef = useRef();
   const ambienteInputRef = useRef();
+  const notify = () => toast.success("Seu relatório foi criado com sucesso!");
+
   const submitHandler = async (event) => {
     event.preventDefault();
     const ambienteValue = ambienteInputRef.current.value;
@@ -18,18 +20,17 @@ export default function Home({ children }) {
     var timestamp = new Date().getTime();
     var date = new Date(timestamp);
     var formattedDate = date.toISOString();
-    
+
     const generateNewId = () => {
       const maxNumber = Math.pow(10, 2) - 1;
       const newId = Math.floor(Math.random() * maxNumber);
-      const formattedId = newId.toString().padStart(2, '0');
+      const formattedId = newId.toString().padStart(2, "0");
       return formattedId;
     };
     const newId = generateNewId(2);
-    console.log(newId);    
-    
+    console.log(newId);
+
     const relatorioData = {
-      id: 16,
       nome_local: ambienteValue,
       endereco: addressValue,
       data: formattedDate,
@@ -83,7 +84,8 @@ export default function Home({ children }) {
               <WebSocketComponent />
             </div>
             <div className={styles.buttons}>
-              <button form="forms" className={styles.button}>
+              <Toaster position="bottom-right" reverseOrder={false} />
+              <button onClick={notify} form="forms" className={styles.button}>
                 Gerar Relatório
               </button>
               <button className={styles.button}>
